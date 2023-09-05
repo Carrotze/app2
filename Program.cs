ad6bafc88e97d681e00f9eb865e00a89ac4d9302
Carrotze <epmkuehn+github@gmail.com>
2023-09-05 19:37:00 +0200

    gfd

--- Program.cs
+++ Program.cs
@@ -2,32 +2,48 @@
 
 class Program
 {
-    public static void Main(string[] args)
+  public static void Main(string[] args)
+  {
+
+    Dice dicey = new();
+    void MainMenu()
     {
-		
-		Dice dicey = new();
-        void MainMenu()
-        {
-			//---------------Declared Variables--------------------
-			bool inputbool = false; //used in the while-loop to check user input
-			string userinput;
-			//---------------Displayed Lines-----------------------
-            Console.WriteLine("DnD Character Sheet by @Carrotze");
-            Console.WriteLine();
-            Console.WriteLine("1. Create Character");
-            Console.WriteLine("2. Load Character");
-			      Console.WriteLine("3. Roll some dice");
+      //---------------Declared Variables--------------------
+int selector;
+      //---------------Displayed Lines-----------------------
+      Console.WriteLine("DnD Character Sheet by @Carrotze");
+      Console.WriteLine();
+      Console.WriteLine("1. Create Character");
+      Console.WriteLine("2. Load Character");
+      Console.WriteLine("3. Roll some dice");
+      Console.WriteLine();
+      Console.WriteLine("Please enter the number of the action you want to use.");
+      selector = Uinput.fIntInput(3);
+      switch (selector)
+      {
+        case 1:
+          Console.WriteLine("Placeholder Create Character");
+          break;
+        case 2:
+          Console.WriteLine("Placeholder Load Character");
+          break;
+        case 3:
+          dicey.fRollDice();
+          break;
+      }
 
-			//--------------Loop for User Input-------------------
-          
-        }
-		void selector()
-		{
 
 
-			
-		}
-		dicey.fRollDice();
-		//MainMenu();
-	}
+      //--------------Loop for User Input-------------------
+
+    }
+    void selector()
+    {
+
+
+
+    }
+    dicey.fRollDice();
+    //MainMenu();
+  }
 }
